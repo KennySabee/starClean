c21a9bc09da56435262315a2744f7c2984b5a6c5
KennySabee <komerizalde@gmail.com>
2024-06-18 14:59:21 -0500

    feat: correciones1

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
 // import Hero from "@/components/Hero2";
 import Hero from "@/components/Hero3";
 import Caracteristicas from "@/components/Caracteristicas";
-import Productos from "@/components/Productos";
 import PreguntasFrecuentes from "@/components/PreguntasFrecuentes";
 import Footer from "@/components/Footer";
 import BotonWp from "@/components/BotonWP";
-import Store from "@/components/StoreFront";
+
 
 export default function Home() {
   return (
@@ -15,7 +14,7 @@ export default function Home() {
       <PreguntasFrecuentes />
       <Footer />
       <BotonWp />
-      {/* <Store/> */}
+      
 
     </>
   );
